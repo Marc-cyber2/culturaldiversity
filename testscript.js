7333a18c9a31319030906375572d5a17f623c668
Marc-cyber2 <manang@guilford.edu>
2025-04-09 20:29:12 -0400

    change

--- testscript.js
+++ testscript.js
@@ -1,18 +1,27 @@
-myitem = document.getElementById("firsttest");
+// Get the modal, images, and close button elements
+const modal = document.getElementById('modal');
+const modalImage = document.getElementById('modal-image');
+const closeModal = document.getElementById('close-modal');
 
-myitem.addEventListener("click", onClick);
-//Creates a response for when you click on where the code is being used
-function onClick() {  
-    myitem.style.color = "purple";  
-    myitem.style.fontSize = "20px"
-//Changes the color and size, once you click where the code is implemented 
-}
+// Get all gallery images
+const galleryImages = document.querySelectorAll('.gallery-image');
 
-myitem.addEventListener("mouseout", onmousemove);
+// Add event listeners for each image to open in the modal
+galleryImages.forEach((image) => {
+  image.addEventListener('click', () => {
+    modal.style.display = 'flex';
+    modalImage.src = image.src; // Set the modal image to the clicked image
+  });
+});
 
-function onmousemove() {  
-        myitem.style.color = "black";  
-        myitem.style.fontSize = "1em"
-//Once you move the mouse of the designated area it will result back to its origin
-}
+// When the user clicks the close button, close the modal
+closeModal.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
 
+// When the user clicks anywhere outside the modal image, close the modal
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
